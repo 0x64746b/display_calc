addea28428ffe6c844d9b2bc37ea4d23249c2fc8
dtk <dtk@gmx.de>
2015-07-14 16:15:47 +0200

    Parse command line arguments

--- display_size.py
+++ display_size.py
@@ -2,12 +2,16 @@
 # coding: utf-8
 
 
+"""Calculate the size of a display from its resolution and diameter."""
+
+
 from __future__ import (
     division,
     print_function,
 )
 
 
+import argparse
 import math
 
 
@@ -21,9 +25,17 @@ def calculate_size(resolution, diameter):
 
 
 if __name__ == '__main__':
-    resolution = (2560, 1600)
-    diameter = 10.055
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument(
+        '-r',
+        '--resolution',
+        metavar=('WIDTH', 'HEIGHT'),
+        type=int,
+        nargs=2,
+    )
+    parser.add_argument('-d', '--diameter', type=float)
+    args = parser.parse_args()
 
-    display_size = calculate_size(resolution, diameter)
+    display_size = calculate_size(args.resolution, args.diameter)
 
     print('Display size: {} x {}'.format(display_size[0], display_size[1]))
